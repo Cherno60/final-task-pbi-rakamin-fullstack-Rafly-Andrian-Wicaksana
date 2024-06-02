746f0a2116936b8e61bd4c0b736bdc0a904c2562
Rafly Andrian <raflyandrian@gmail.com>
2024-06-02 20:57:30 +0700

    Simplify the env variables

--- database/db.go
+++ database/db.go
@@ -12,6 +12,7 @@ var DB *gorm.DB
 func DBConnect() {
 	var err error
 	dsn := os.Getenv("DB_URL")
+	//fmt.Println(dsn)
 	//fmt.Println(os.Getenv("SECRET_KEY"))
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	//Created By Rafly Andrian
